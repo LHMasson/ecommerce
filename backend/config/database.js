fdbc01d41ec34aa08e23fbbc15b0b4e95eb285f9
Lucas Masson <lucas.masson001@gmail.com>
2023-02-01 20:58:33 -0300

    Commit 4

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,13 +1,12 @@
 const mongoose = require('mongoose');
 
-
+mongoose.set("strictQuery", true);
 const connectDatabase = () => {
     mongoose.connect(process.env.DB_LOCAL_URI, {
         useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true
-    }).then(con =>{
-        console.log(`MongoDB Database connected with HOST: #{con.connection.host}`)
+        useUnifiedTopology: true
+    }).then(con => {
+        console.log(`MongoDB Database connected with HOST: ${con.connection.host}`)
     })
 };
 
