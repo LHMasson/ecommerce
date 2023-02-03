f5adfa9f7ddd15f1f74312fcf054696816a7cc41
Lucas Masson <lucas.masson001@gmail.com>
2023-02-03 10:21:51 -0300

    Commit - DeleteProduct

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -40,7 +40,7 @@ exports.getSingleProduct = async (req, res, next) => {
 
 }
 
-// Update Product => /api/v1/product/:id
+// Update Product => /api/v1/admin/product/:id
 exports.updateProduct = async (req, res, next) => {
 
     let product = await Product.findById(req.params.id);
@@ -61,5 +61,26 @@ exports.updateProduct = async (req, res, next) => {
         success: true,
         product
     });
+
+}
+
+// Delete Product => /api/v1/admin/product/:id
+exports.deleteProduct = async (req, res, next) => {
+
+    const product = await Product.findById(req.params.id);
+
+    if(!product) {
+        return res.status(404).json({
+            success: false,
+            message: 'Product not found' 
+        })
+    }
+
+    await product.remove();
+
+    res.status(200).json({
+        success: true,
+        message: 'Product is deleted'
+    })
 
 }
