ee01f67c48e7e5a2eb96cf817c2cca5faba449e2
Lucas Masson <lucas.masson001@gmail.com>
2023-02-05 12:39:02 -0300

    Filtering Products

--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -15,6 +15,23 @@ class APIFeatures {
         this.query = this.query.find({ ...keyword });
         return this;
     }
+
+    filter() {
+
+        const queryCopy = { ...this.queryStr };
+
+        // Removing Fields From Query
+        const removeFields = ['keyword', 'limit', 'page']
+        removeFields.forEach(el => delete queryCopy[el]);
+
+        // Advance Filter For Price, Ratings, etc
+        let queryStr = JSON.stringify(queryCopy)
+        queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, match => `$${match}`)
+
+        this.query = this.query.find(JSON.parse(queryStr));
+        return this;
+    }
+
 }
 
 module.exports = APIFeatures
