2cd4eb5fc51436dfd643b77fb26daeb8713fd690
Lucas Masson <lucas.masson001@gmail.com>
2023-02-05 10:29:28 -0300

    Search Functionalities

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,6 +2,7 @@ const Product = require('../models/product');
 
 const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
+const APIFeatures = require('../utils/apiFeatures')
 
 // Create New Product => /api/v1/product/new
 exports.newProduct = catchAsyncErrors (async (req, res, next) => {
@@ -14,10 +15,13 @@ exports.newProduct = catchAsyncErrors (async (req, res, next) => {
     })
 })
 
-// Get All Products => /api/v1/products
+// Get All Products => /api/v1/products?keyword=apple
 exports.getProducts = catchAsyncErrors (async (req, res, next) => {
     
-    const products = await Product.find();
+    const apiFeatures = new APIFeatures(Product.find(), req.query)
+        .search()
+
+    const products = await apiFeatures.query;
 
     res.status(200).json({
         success: true,
